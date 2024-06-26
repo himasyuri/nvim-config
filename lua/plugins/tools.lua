4dd1d80a279b055ea38b5b0c4ea89f110ccda917
Ann Vasileva <a.vasileva@bpo.travel>
2024-06-26 17:29:55 +0300

    added folds and useful tools

---
+++ lua/plugins/tools.lua
@@ -0,0 +1,47 @@
+local config = require "env.config.tools"
+return {
+  {
+    "folke/todo-comments.nvim",
+    event = "BufRead",
+    opts = function()
+      return config.todoComments
+    end,
+    config = function(_, opts)
+      require("todo-comments").setup(opts)
+    end,
+  },
+
+  {
+    "folke/trouble.nvim",
+    event = "LspAttach",
+    keys = function()
+      return config.troubleKeys()
+    end,
+    cmd = { "Trouble", "TroubleToggle" },
+    opts = function()
+      return config.trouble
+    end,
+    config = function(_, opts)
+      require("trouble").setup(opts)
+    end,
+  },
+  {
+    "folke/which-key.nvim",
+    keys = { "<leader>", "<c-r>", "<c-w>", '"', "'", "`", "c", "v", "g" },
+    cmd = "WhichKey",
+    opts = {},
+    config = function(_, opts)
+      require("which-key").setup(opts)
+    end,
+  },
+  {
+    "folke/zen-mode.nvim",
+    cmd = { "ZenMode" },
+    opts = function()
+      return config.zenMode
+    end,
+    config = function(_, opts)
+      require("zen-mode").setup(opts)
+    end,
+  },
+}
