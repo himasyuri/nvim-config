07604464a3938c6bb8c7e12224d3f2c29b5e1d23
Ann Vasileva <a.vasileva@bpo.travel>
2024-06-26 16:32:20 +0300

    added terminal and statusline

---
+++ lua/plugins/terminal.lua
@@ -0,0 +1,16 @@
+local config = require "env.config.terminal"
+
+return {
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  lazy = false,
+  keys = function()
+    return config.keys()
+  end,
+  opts = function()
+    return config.opts
+  end,
+  config = function(_, opts)
+    require("toggleterm").setup(opts)
+  end,
+}
